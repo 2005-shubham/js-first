e1316c1dd465189b18b9a1f4d1ac02c4440dc428
2005-shubham <shubhamsharmaalwar89@gmail.com>
2024-06-10 13:44:48 +0530

    basic of js

---
+++ index.js
@@ -0,0 +1,61 @@
+// var x=10;
+// console.log(x);
+// function add(a){     //parameter
+//     console.log(a)
+// }
+// add(20);            //argument
+
+
+// function add(...a){     //parameter
+//     console.log(a);
+// }
+// add(10,20,30);          //argument
+
+// function add(...a){     //parameter
+//     console.log(a[0])
+// }
+// add(10,20,30);          //argument
+
+// function add(...a){     //parameter
+//     console.log(a[1])
+// }
+// add(10,20,30);          //argument
+
+// function add(...a){     //parameter
+//     console.log(a[2])
+// }
+// add(10,20,30);          //argument
+
+// function add(...a){     //parameter
+//     console.log(a[3])
+// }
+// add(10,20,30);          //argument
+
+// function add(a){
+//     console.log(a);
+// }
+// const result = add("5");
+
+// let str="hello I Love U";
+// console.log(str[3]);
+// console.log(str.slice(0,7));
+// console.log(str.length);
+
+// const y={"key":"value","first name":"I Love U"};
+// // square brackets used when there is space in key string...........
+// console.log(y["key"]);
+// console.log(y["first name"]);
+
+// const z={"key":"val","first name":"I Hate Love story"};
+// console.log(z);
+
+// const arr=["hello","one",{"firstname":"hello","secondname":"surname"},[120,140,[4,5,6]]];
+
+// milk,sugar,coffee
+
+let milk = 100;
+let sugar = 20;
+let coffee = 50;
+coffee-making = milk * sugar * coffee;
+
+const coffee-making
