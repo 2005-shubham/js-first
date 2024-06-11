6d32921868802fa31be45d5c3bb21c08ebc62ba9
2005-shubham <shubhamsharmaalwar89@gmail.com>
2024-06-11 11:21:47 +0530

    JS is complect

--- index.js
+++ index.js
@@ -51,11 +51,113 @@
 
 // const arr=["hello","one",{"firstname":"hello","secondname":"surname"},[120,140,[4,5,6]]];
 
-// milk,sugar,coffee
+// milk,sugar,coffee_powder
 
-let milk = 100;
-let sugar = 20;
-let coffee = 50;
-coffee-making = milk * sugar * coffee;
+// let milk = 100;
+// let sugar = 20;
+// let coffee_powder = 50;
+// let coffee = milk * sugar * coffee_powder;
+// const count = 5;
+// coffee = count * (milk * sugar * coffee_powder) / 170;
+// console.log(coffee);
 
-const coffee-making
+// let name1 = "it's hello"
+// console.log(name1);
+
+// let name1 = 'it"s hello'
+// console.log(name1);
+
+// let ischeck = "true"
+// console.log(ischeck);
+
+// let person ={"key":10,"str":"hello","bool":true,};
+// console.log(person["bool"]);
+
+// function
+// add(num){
+//     console.log("my poket money is " +num+ " in current time");
+// }
+// add(100)
+
+// function
+// add(num){
+//     console.log("my poket money is " +(num+num)+ " in current time");
+// }
+// add(100)
+
+// function add(num){
+//     return num;
+// }
+// const result = add(100);
+// console.log(result);
+
+// let age = 18;
+// if ( age <= 18){
+//     console.log("vote");
+// }
+
+// let age = 18;
+// if ( age > 18){
+//     console.log("vote");
+// }
+// else {
+//     console.log("not able for vote");
+// }
+
+// let age = 18;
+// if ( age > 18)
+// {
+//     console.log("greater than 18");
+// }
+// else if (age == 18) 
+// {
+//     console.log("not able for vote");
+// } 
+// else 
+// {
+//     console.log("Is equal to 18");
+// }
+
+// let str = "monday";
+// switch (str){
+//         case "monday":
+//         console.log("today is Monday");
+//         break;
+//         case "Tuesday":
+//         console.log("today is Tuesday");
+//         case "Wednesday":
+//         console.log("today is Wednesday");
+//         case "Thurday":
+//         console.log("today is Thurday");
+//         case "Friday":
+//         console.log("today is Friday");
+//         case "Satarday":
+//         console.log("today is Satarday");
+//         case "sunday":
+//         console.log("today is Sunday");
+        
+// }
+// let i = 10;
+//  while(i >= 0){
+//     console.log(2*i);
+//     --i;
+//  }
+
+// let i = 0;
+// do{
+//     console.log(2*i);
+//     i++;
+// }
+// while (i<=10);
+
+// let i;
+// for(i = 1; i <=10; i++){
+//     console.log(`2 X ${i} = ${2*i}`);
+// }
+
+let date = new Date();
+console.log(date.getFullYear())
+console.log(date.getDate())
+console.log(date.getMonth())
+console.log(date.getHours())
+console.log(date.getMinutes())
